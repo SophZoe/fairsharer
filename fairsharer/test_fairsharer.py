982df59bfad8bd1d3e712a6ed0a2fbdda76daa21
SophZoe <sophie.paul@study.hs-duesseldorf.de>
2023-12-06 07:21:26 +0100

    added pytest_function

--- fairsharer/test_fairsharer.py
+++ fairsharer/test_fairsharer.py
@@ -2,3 +2,12 @@ import pytest
 
 from fair_sharer import fair_sharer
 
+@pytest.mark.parametrize("values, num_iterations, share, expected_result",
+                         [([0, 1000, 800, 0], 1, 0.1, [100, 800, 900, 0]),
+                          ([0, 1000, 800, 0], 2, 0.1, [100, 890, 720, 90]),
+                          ([0, 1000, 800, 0], 3, 0,1, [189, 712, 809, 90])
+                          ])
+
+def test_fair_sharer(values, num_iterations, share, expected_result):
+    result = fair_sharer(values, num_iteration, share)
+    assert result == expected_result
